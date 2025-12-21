f97e2953236909cc2dfc1b39d61ec148d36f0c0d
MoazRyhan <moazryhan@gmail.com>
2025-12-21 13:31:01 +0200

    ready to work after company model and fix error2

--- src/DB/Models/CompanyModel.js
+++ src/DB/Models/CompanyModel.js
@@ -7,6 +7,12 @@ import {
 
 const CompanySchema = new mongoose.Schema(
   {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    
     /* ================= BASIC INFO ================= */
     name: { type: String, required: true },
 
